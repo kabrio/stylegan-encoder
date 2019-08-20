3f011b06efc66973a456cb227fb704e421b3784f
Jonas Bo <jbohatsch@gmail.com>
2019-08-20 13:17:45 +0200

    added runway module functions

---
+++ runway_model.py
@@ -0,0 +1,11 @@
+import os
+import pickle
+import PIL.Image
+import numpy as np
+import dnnlib
+import dnnlib.tflib as tflib
+import config
+from encoder.generator_model import Generator
+
+import matplotlib.pyplot as plt
+%matplotlib inline
